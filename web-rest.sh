08528008e4b80a20e9523c88aa727252a4399b53
aaa20 <41234362+aaa20@users.noreply.github.com>
2021-05-10 15:40:08 +0500

    Update web-rest.sh

--- web-rest.sh
+++ web-rest.sh
@@ -2,14 +2,18 @@
 
 
 sudo apt update
-sudo cp -rv /web_bak/html/ /var/www/
+sudo cp -r /web_bak/html /var/www/html
 sudo cp -rv /web_bak/etc/php /etc/
 sudo cp -rv /web_bak/etc/nginx /etc/
 
 sudo apt -y install nginx
 sudo systemctl enable nginx
+sudo systemctl start nginx.service
+
+
 sudo apt -y install php7.4 php7.4-mysql php7.4-cli php7.4-fpm
 sudo systemctl enable php7.4-fpm.service
+sudo systemctl start php7.4-fpm.service
 
 curl http://127.0.0.1
 
